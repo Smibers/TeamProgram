d7ec67e1a62a5fb7196823b73d3d3c5866ba7d03
K-CPS <113073406+K-CPS@users.noreply.github.com>
2023-11-21 14:27:11 -0500

    updated kotlin and jdk
    update kotlin from 1.6.2 to 1.8.22 in order to get updated dependencies for navigation graphs, also upgraded jdk from 1.8 to 17

---
+++ Kayvon/ChaChing/app/src/main/java/com/cps298/chaching/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.cps298.chaching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cps298.chaching.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
